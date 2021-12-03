cf9b9bed5b954632b923e5168725e0b9057a3893
aishrath <51309797+aishrath@users.noreply.github.com>
2021-12-03 04:12:38 -0500

    tables for requests and complaints

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ conn.execute('CREATE TABLE Inventory (Kind VARCHAR(50), Name VARCHAR(50), Amount
 conn.execute('CREATE TABLE ShoppingCart (Kind VARCHAR(50), Name VARCHAR(50), Amount INTEGER, Cost DECIMAL, Tax FLOAT)')
 conn.execute('CREATE TABLE Receipt (Kind VARCHAR(50), Name VARCHAR(50), Amount INTEGER, Cost DECIMAL, Tax FLOAT)')
 conn.execute('CREATE TABLE Review (Name VARCHAR(50), Rating FLOAT, Review VARCHAR(500))')
+conn.execute('CREATE TABLE Complaints (Complaint VARCHAR(500), Time DATETIME)')
+conn.execute('CREATE TABLE Requests (Name VARCHAR(50), Kind VARCHAR(50))')
 
 conn.commit()
 conn.close()
