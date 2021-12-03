7905a52ec7865590e2d3e4165d1a9f245a790a24
aishrath <51309797+aishrath@users.noreply.github.com>
2021-12-03 02:48:25 -0500

    added table for reviews

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ conn = sqlite3.connect('groceryData.db')
 conn.execute('CREATE TABLE Inventory (Kind VARCHAR(50), Name VARCHAR(50), Amount INTEGER, Cost DECIMAL)')
 conn.execute('CREATE TABLE ShoppingCart (Kind VARCHAR(50), Name VARCHAR(50), Amount INTEGER, Cost DECIMAL, Tax FLOAT)')
 conn.execute('CREATE TABLE Receipt (Kind VARCHAR(50), Name VARCHAR(50), Amount INTEGER, Cost DECIMAL, Tax FLOAT)')
+conn.execute('CREATE TABLE Review (Name VARCHAR(50), Rating FLOAT, Review VARCHAR(500))')
 
 conn.commit()
 conn.close()
